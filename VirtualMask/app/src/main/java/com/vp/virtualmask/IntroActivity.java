086dd9f8beb99e85128c06fe1d101094bb637916
Vishal Kothari <vishal.kk@somaiya.edu>
2020-07-09 21:00:27 +0530

    added the base gui

--- VirtualMask/app/src/main/java/com/vp/virtualmask/IntroActivity.java
+++ VirtualMask/app/src/main/java/com/vp/virtualmask/IntroActivity.java
@@ -6,6 +6,8 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -15,6 +17,13 @@ public class IntroActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_intro);
 
         button = (Button) findViewById(R.id.gender_selected);
@@ -50,7 +59,7 @@ public class IntroActivity extends AppCompatActivity {
             toast.show();
         }
         else {
-            Intent intent = new Intent(this, MainActivity.class);
+            Intent intent = new Intent(this, FullscreenActivity.class);
             startActivity(intent);
             finish();
         }
--- VirtualMask/app/src/main/java/com/vp/virtualmask/SplashScreen.java
+++ VirtualMask/app/src/main/java/com/vp/virtualmask/SplashScreen.java
@@ -5,13 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashScreen extends AppCompatActivity {
 
-    private static final long SPLASH_TIME_OUT = 5000;
+    private static final long SPLASH_TIME_OUT = 7000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash_screen);
 //        if (getSupportActionBar() != null) {
 //            getSupportActionBar().hide();
